a2bf13e94729ae9a8ee500c0a8efec3b4a1912bd
jtrefil <jirka.trefilu@gmail.com>
2024-12-04 13:32:14 +0100

    second task

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 mod io;
 
 fn parse_file_content(file_content: &String) -> (Vec<i32>,Vec<i32>){
@@ -10,13 +12,14 @@ fn parse_file_content(file_content: &String) -> (Vec<i32>,Vec<i32>){
         list1.push(items[0].parse::<i32>().unwrap());
         list2.push(items[1].parse::<i32>().unwrap());
     }
-    list1.sort();
-    list2.sort();
-
+  
     return (list1, list2);
 }
 
-fn compute_list_diff(list1: Vec<i32>, list2: Vec<i32>) -> i32{
+fn compute_list_diff(list1: &mut Vec<i32>, list2: &mut Vec<i32>) -> i32{
+    list1.sort();
+    list2.sort();   
+
     let mut diff: i32= 0;
     
     for i in 0..list1.len(){
@@ -26,10 +29,35 @@ fn compute_list_diff(list1: Vec<i32>, list2: Vec<i32>) -> i32{
     return diff;
 }
 
+fn compute_list_similarity(list1: Vec<i32>, list2: Vec<i32>) -> i32{
+    let mut freq_map: HashMap<i32, i32>= std::collections::HashMap::new();
+    for i in 0..list2.len(){
+        let val = freq_map.get(&list2[i]);
+        if val.is_none(){
+            freq_map.insert(list2[i], 1);
+            continue;
+        }
+        freq_map.insert(list2[i], val.unwrap() + 1);
+    }
+    let mut similarity: i32 = 0;
+
+    for i in 0..list1.len(){
+        let item = freq_map.get(&list1[i]);
+        let freq: &i32 = if item.is_none(){&0} else {item.unwrap()}; 
+        similarity += list1[i] * (*freq);
+    }
+    return similarity;
+
+}
+
 fn main() {
     const X: &str = "input.txt";
     let file_content = io::filesystem::read_file(X);
-    let two_lists = parse_file_content(&file_content);
-    let diff: i32 = compute_list_diff(two_lists.0, two_lists.1);
-    println!("{diff}")
+    let mut two_lists = parse_file_content(&file_content);
+
+    let diff: i32 = compute_list_diff(&mut two_lists.0, &mut two_lists.1);
+    println!("list diff: {diff}");
+    let similarity = compute_list_similarity(two_lists.0, two_lists.1);
+    println!("list similarity: {similarity}")
+
 }
