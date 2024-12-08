4eda1a2ebce5a4da2c4946dfb09c2c73d83b0f44
jtrefil <jirka.trefilu@gmail.com>
2024-12-08 16:17:53 +0100

    day4 part2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,5 +1,6 @@
 const TARGET: &str = "XMAS";
-
+const TARGET2: &str = "MAS";
+const TARGET2_REVERSED: &str = "SAM";
 fn read_input(path: &str)->String{
     return std::fs::read_to_string(path).expect("FileNotFound");
 }
@@ -136,11 +137,55 @@ fn count_xmas_in_matrix(matrix: Vec<Vec<char>>) -> u32{
 }
 
 
+fn check_forward_diagonal_2(matrix:&Vec<Vec<char>>, position: (usize,usize)) -> bool {
+    let row = position.0;
+    let column = position.1;
+    let n = matrix.len();
+    let mut str: String = String::with_capacity(3);
+    let steps = std::cmp::min(std::cmp::min(n - row, n - column), 3);
+    for i in  0..steps{
+        str.push(matrix[row+i][column+i]);
+    }
+    return is_target_2(&str);
+}
 
+fn check_backward_diagonal_2(matrix:&Vec<Vec<char>>, position: (usize,usize)) -> bool{
+    let row = position.0;
+    let column = position.1;
+    let mut str: String = String::with_capacity(3);
+
+    let steps = 3;//std::cmp::min(3);
+    for i in  0..steps{
+        str.push(matrix[row-i][column+i]);
+    }
+    return is_target_2(&str);
+}
+
+fn is_target_2(str: &str) -> bool {
+    return TARGET2 == str || TARGET2_REVERSED == str;
+}
+
+fn count_mas_in_matrix(matrix: Vec<Vec<char>>) -> u32{
+    let dim = matrix.len();
+    let mut count: u32 = 0;
+    for i in 0..dim - 2{
+        for j in 0..dim{
+            let c = matrix[i][j];
+            let position = (i,j);
+            if c == 'M' || c == 'S'{
+                let position2 = (position.0+2, position.1);
+                if check_forward_diagonal_2(&matrix, position) && check_backward_diagonal_2(&matrix, position2) {
+                    count += 1;
+                }
+            }
+        } 
+    }
+    return count;
+}
 
 fn main() {
     let input = read_input("input.txt");
     let matrix = create_char_matrix(&input);
-    let count = count_xmas_in_matrix(matrix);
+    let count = count_mas_in_matrix(matrix);
     println!("xmas count: {count}");
 }
