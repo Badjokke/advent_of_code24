562c9bdf00ae765bfe240925e05f2adafc1177b8
badjokke <badjokke@gmail.com>
2024-12-24 23:16:07 +0100

    day6 part 1 finished

--- day_6/src/map/map.rs
+++ day_6/src/map/map.rs
@@ -29,24 +29,30 @@ impl Map{
     }
     
     fn is_out_of_map(&self, i: i32, j: i32) -> bool {
-         (i < 0 || i == self.map.len() as i32) || (j < 0 || j == self.map[i as usize].len() as i32)
+         (i < 0 || i == self.map.len() as i32) || (j < 0 || j == self.map.len() as i32)
     }
     
-    fn add_distinct_position(&mut self, i: usize, j: usize) {
-        let tmp = i.to_string() + &j.to_string();
-        self.visited_tiles.insert(tmp);
-    }
 
     fn is_obstruction(&self, i: usize, j: usize) -> bool {
         self.map[i][j] == self.obstruction
     }
+    fn print_map(&self){
+        for i in 0..self.map.len(){
+            println!("{:?}",self.map[i]);
+        }
+    }
 
-    pub fn simulate(&mut self) -> usize{
+
+    pub fn simulate(&mut self) -> u32{
         let starting_position = self.find_guard_starting_position();
         let mut i = starting_position.0;
         let mut j = starting_position.1;
+        let mut visited:u32 = 0;
         loop {
-            self.add_distinct_position(i,j);
+            if self.map[i][j] != 'X'{
+                self.map[i][j] = 'X';
+                visited += 1;
+            }
             let mut tmp: (i32, i32) = self.guard.step(i as i32, j as i32);
             if self.is_out_of_map(tmp.0, tmp.1){
                 break;
@@ -57,8 +63,9 @@ impl Map{
             }
             i = tmp.0 as usize;
             j = tmp.1 as usize;
-        }        
-        self.visited_tiles.len()
+        }
+        self.print_map();
+        visited
     }
 
 
